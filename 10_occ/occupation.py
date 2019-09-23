7e60cb577937d17098fb52db794f64ea1d7b2375
Calvin Chu <cchu00@stuy.edu>
2019-09-23 14:49:22 -0400

    comments

--- 10_occ/occupation.py
+++ 10_occ/occupation.py
@@ -3,14 +3,15 @@ from csv import DictReader as dr
 from random import choices as rc
 from collections import OrderedDict
 
+#takes in a file, creates and returns a dictionary reading each row in the csv and matching job to percent
 def getoccupations(file):
   dic = {}
   for row in dr(open(file)):
     dic[row["Job Class"]] = float(row["Percentage"])
   return dic
-
+#copies dict and removes Total, unzips keys + values and assigns them to variables, randomchoice keys+values based on %
 def randoccupation(dic):
-  tmp = OrderedDict(dic.copy())
+  tmp = dic.copy()
   tmp.pop("Total")
   keys, values = zip(*tmp.items())
   return rc(keys, values)[0]
