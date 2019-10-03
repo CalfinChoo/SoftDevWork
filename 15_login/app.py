5128f3d2bd7b94893edb628ce57a4910954c52ed
Calvin Chu <cchu00@stuy.edu>
2019-10-03 14:56:13 -0400

    classwork 2

--- 15_login/app.py
+++ 15_login/app.py
@@ -4,34 +4,48 @@
 #2019-10-02
 
 from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
 
+app = Flask(__name__)
+submitted = False
+print("loen")
 # Instantiates a variable to contain a potential error message
 error = ""
+
 
 # Root serves as our login page
 @app.route("/")
 def makeForm():
     global error
+    global submitted
     # If there is an error message, reset the variable and display the login page with the error message
     if (error != ""):
+        submitted = True
         message = error
         error = ""
         return render_template('app.html', e = message)
     # Else display the login page normally
     else :
+        submitted = True
         return render_template('app.html')
 
 # /login route serves as welcome page
 @app.route("/login")
 def authenticate():
     global error
+    global submitted
     # Hardcoded a username and password
     use = "giraffe"
     pas = "g"
     # Stores user's inputs from login page
-    username = request.args["user"] + ""
-    password = request.args["pass"] + ""
+    print(submitted)
+    if submitted:
+        username = request.args["user"] + ""
+        password = request.args["pass"] + ""
+    else:
+        #print("11738271698127369187612786129786129837126983712678126789163891273612789361278")
+        return redirect(url_for('makeForm'))
+        #print("444444444444444444444")
+
     # If inputs match hardcoded username and password, reset error message and display welcome page with logout button
     # **Logout button reroutes to root**
     if(username == use and password == pas):
@@ -41,6 +55,7 @@ def authenticate():
     # Else set an error message and redirect back to root
     else:
         error = "Wrong username or password"
+        submitted = False
         return redirect(url_for('makeForm'))
 
 
