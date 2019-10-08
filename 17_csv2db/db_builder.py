257f262651f7acf02439ab3e7bb43831c15fc120
Calvin Chu <cchu00@stuy.edu>
2019-10-08 14:54:40 -0400

    classwork

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -13,7 +13,16 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
-
+files = ['students.csv', 'courses.csv']
+def createTbl(file):
+    with open(file) as csvfile:
+        scanner = csv.reader(csvfile, delimiter = ',')
+        fields = next(scanner)
+        c.execute('CREATE TABLE {} (? TEXT, ? INTEGER, ? INTEGER PRIMARY KEY)'.format(file), [fields[0], fields[1], fields[2]])
+        for row in scanner:
+            c.execute('INSERT INTO ? VALUES (?, ?, ?)', [file, row[0], row[1], row[2]])
+for file in files:
+    createTbl(file)
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
 
 
