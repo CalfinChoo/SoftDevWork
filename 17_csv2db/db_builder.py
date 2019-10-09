499d455ec6f480005f1e78aca0150263e6da9b35
Calvin Chu <cchu00@stuy.edu>
2019-10-09 01:23:32 -0400

    worked on assignment

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,6 +1,6 @@
-#Clyde "Thluffy" Sinclair
+#Calvin Chu
 #SoftDev
-#skeleton :: SQLITE3 BASICS
+#My Populated Skeleton :: SQLITE3 BASICS
 #Oct 2019
 
 import sqlite3   #enable control of an sqlite database
@@ -13,23 +13,34 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
-files = ['students.csv', 'courses.csv']
-def createTbl(file):
-    with open(file) as csvfile:
-        scanner = csv.reader(csvfile, delimiter = ',')
-        fields = next(scanner)
-        c.execute('CREATE TABLE {} (? TEXT, ? INTEGER, ? INTEGER PRIMARY KEY)'.format(file), [fields[0], fields[1], fields[2]])
-        for row in scanner:
-            c.execute('INSERT INTO ? VALUES (?, ?, ?)', [file, row[0], row[1], row[2]])
-for file in files:
-    createTbl(file)
+
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
 
+#Drop tables if they already exist
+c.execute('DROP TABLE IF EXISTS students')
+c.execute('DROP TABLE IF EXISTS courses')
+#Create tables if they do
+#Open corresponding csv file, make a DictReader and insert the values in each row (each row being a dictionary) of the DictReader
+c.execute('''CREATE TABLE IF NOT EXISTS students (name TEXT, age INTEGER, id INTEGER PRIMARY KEY)''')
+with open('students.csv') as csvfile:
+     scanner = csv.DictReader(csvfile)
+     for row in scanner:
+     	 c.execute('INSERT INTO students VALUES (?, ?, ?)', (row["name"], row["age"], row["id"]))
+c.execute('''CREATE TABLE IF NOT EXISTS courses (code TEXT, mark INTEGER, id INTEGER)''')
+with open('courses.csv') as csvfile:
+     scanner = csv.DictReader(csvfile)
+     for row in scanner:
+         c.execute('INSERT INTO courses VALUES (?, ?, ?)', (row["code"], row["mark"], row["id"]))
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
+#Runs SELECT command and prints all results in tables
+command = "SELECT * FROM students"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
+print(c.fetchall())
+c.execute('SELECT * FROM courses')
+print(c.fetchall())
 
 #==========================================================
 
 db.commit() #save changes
+
 db.close()  #close database
