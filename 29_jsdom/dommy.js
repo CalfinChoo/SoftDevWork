5af62c4ce235a3e635a9c3c877dc245cfb91a623
Calvin Chu <cchu00@stuy.edu>
2019-12-13 14:58:33 -0500

    class

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -11,29 +11,24 @@ var addItem = function(e) {
   var item = document.createElement("li");
   item.innerHTML = "WORD";
   list.appendChild(item);
-  item.addEventListener('mouseover', function(e){changeHeading(e.target.innerText)});
+  item.addEventListener('mouseover', function(e){changeHeading(this.innerText)});
   item.addEventListener('mouseout', function(e){changeHeading("Hello World!")});
-  item.addEventListener('click', function(e){removeItem(e.target); changeHeading("Hello World!");});
+  item.addEventListener('click', function(e){removeItem(this); changeHeading("Hello World!");});
 };
 var changeHeading = function(e) {
   var h = document.getElementById("h");
   h.innerHTML = e;
 };
 var removeItem = function(e) {
-  var list = document.getElementById("thelist");
-  var items = document.getElementsByTagName("li");
-  var rem;
-  for (var i = 0; i < items.length; i++) {
-    if (items[i] == e) rem = items[i];
-  }
-  list.removeChild(rem);
+  e.remove();
 };
 
 var lis = document.getElementsByTagName("li");
 for (var i = 0; i < lis.length; i++) {
-  lis[i].addEventListener('mouseover', function(e){changeHeading(e.target.innerText)});
+  lis[i].addEventListener('mouseover', function(e){changeHeading(this.innerText)});
+  //lis[i].addEventListener('mouseover', function(e){console.log(e.target)});
   lis[i].addEventListener('mouseout', function(e){changeHeading("Hello World!")});
-  lis[i].addEventListener('click', function(e){removeItem(e.target); changeHeading("Hello World!");});
+  lis[i].addEventListener('click', function(e){removeItem(this); changeHeading("Hello World!");});
 }
 
 var count = 0;
@@ -54,7 +49,7 @@ var addFib = function(e) {
 var addFib2 = function(e) {
   if (count < 3) return addFib(e);
   var list = document.getElementById("fiblist");
-  var nums = document.getElementById("fiblist").childNodes;
+  var nums = list.childNodes;
   var item = document.createElement("li");
   var sum = parseInt(nums[nums.length - 1].textContent) + parseInt(nums[nums.length - 2].textContent);
   console.log(e);
