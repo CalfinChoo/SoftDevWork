cf6a334418a1916bf6b42b00dc0a364a688d2aea
Calvin Chu <cchu00@stuy.edu>
2019-12-15 20:07:45 -0500

    added comments

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -3,9 +3,11 @@
 // K29 -- Sequential Progression III: Season of the Witch
 // 2019-12-12
 
+// Gets button element and calls addItem on click
 var button = document.getElementById("b");
 button.addEventListener('click', function(e){addItem(e)});
 
+// Gets list element, creates a new li and appends it to the list, then sets appropriate EventListeners to new child
 var addItem = function(e) {
   var list = document.getElementById("thelist");
   var item = document.createElement("li");
@@ -15,14 +17,19 @@ var addItem = function(e) {
   item.addEventListener('mouseout', function(e){changeHeading("Hello World!")});
   item.addEventListener('click', function(e){removeItem(this); changeHeading("Hello World!");});
 };
+
+// Changes the heading of the page
 var changeHeading = function(e) {
   var h = document.getElementById("h");
   h.innerHTML = e;
 };
+
+// Removes the element that calls it
 var removeItem = function(e) {
   e.remove();
 };
 
+// Gets the initial list and adds the appropriate EventListeners to them
 var lis = document.getElementsByTagName("li");
 for (var i = 0; i < lis.length; i++) {
   lis[i].addEventListener('mouseover', function(e){changeHeading(this.innerText)});
@@ -31,12 +38,15 @@ for (var i = 0; i < lis.length; i++) {
   lis[i].addEventListener('click', function(e){removeItem(this); changeHeading("Hello World!");});
 }
 
+// Basic fib algorithm
 var count = 0;
 var fib = function(n) {
   if (n == 0) return 0;
   else if (n < 2) return 1;
   else return fib(n-1) + fib(n-2);
 };
+
+// Gets the fiblist, creates a new list element containing the next fib number (using the fib function) and appends it
 var addFib = function(e) {
   console.log(e);
   console.log(fib(count));
@@ -46,6 +56,7 @@ var addFib = function(e) {
   list.appendChild(item);
   count++;
 };
+// Gets the fiblist, creates a new list element containing the next fib number (getting and using the last two numbers generated) and appends it
 var addFib2 = function(e) {
   if (count < 3) return addFib(e);
   var list = document.getElementById("fiblist");
@@ -58,6 +69,8 @@ var addFib2 = function(e) {
   list.appendChild(item);
   count++;
 };
+
+// Gets fib button and calls addFib2 on click
 var fb = document.getElementById("fb");
 //fb.addEventListener('click', function(e){addFib(e)});
 fb.addEventListener('click', function(e){addFib2(e)});
