7c77619ded557dc145d5201ab4516a6fbfbdb359
Calvin Chu <cchu00@stuy.edu>
2020-02-05 19:26:04 -0500

    w

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,7 +1,8 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext('2d');
 var option = 0; // 0 (default = dot)
-// 1 (rect)
+                // 1 (rect)
+var isDrawing = false;
 
 var swap = function(){
     if (option == 0){
@@ -12,11 +13,15 @@ var swap = function(){
     }
 };
 
-var draw = function(){
-  if (option == 0) ctx.fillRect(window.event.clientX, window.event.clientY, 1, 1);
-  // if (option == 1) drawRect(window.event.clientX, window.event.clientY);
+var draw = function(d, mouse){
+  isDrawing = d;
 };
 
+var move = function(){
+  if (isDrawing) {
+    if (option == 1) ctx.fillRect(mouse.clientX, mouse.clientY, 1, 1);
+  }
+};
 // var drawRect = function(x, y) {
 //   if (option == 1) {
 //
@@ -28,5 +33,5 @@ var clear = document.getElementById("clear");
 clear.addEventListener("click", function(){ctx.clearRect(0, 0, c.width, c.height);});
 var toggle = document.getElementById("toggle");
 toggle.addEventListener("click", swap());
-c.addEventListener("mousedown", draw());
-c.addEventListener("mouseup", draw());
+c.addEventListener("mousedown", function(){draw(true, this)});
+window.addEventListener("mouseup", function(){draw(false, this)});
