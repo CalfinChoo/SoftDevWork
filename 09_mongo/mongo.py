e9c519d6173c7831d9bd4669c2ab57711b4e5255
CalfinChoo <cchu00@stuy.edu>
2020-02-27 15:42:43 -0800

    done

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -4,27 +4,35 @@
 #2020-02-26
 
 from pymongo import MongoClient
-import json
 from bson.json_util import loads
+import pprint
 
 client = MongoClient()
 db = client.test
 collection = db.restaurants
-# u = urllib2.urlopen(
-#         "https://raw.githubusercontent.com/WilliamC07/SoftDev/master/09_mongo/primer-dataset.json"
-#     )
-# data = json.loads(u.read())
-f = open("data.json","r")
-response = f.read()
-data = loads(response)
-f.close()
-for post in data:
-    collection.insert_one(post)
+if (not "restaurants" in db.list_collection_names()):
+    f = open("data.json","r")
+    response = f.read()
+    data = loads(response)
+    f.close()
+    for post in data:
+        collection.insert_one(post)
 
 def borough_query(b):
-
+    for x in (collection.find({"borough" : b})):
+        pprint.pprint(x)
 def zip_query(z):
-
+    for x in (collection.find({"address.zipcode" : z})):
+        pprint.pprint(x)
 def zip_grade_query(z, g):
+    for x in (collection.find({"$and" : [{"address.zipcode" : z}, {"grades.grade" : g}]})):
+        pprint.pprint(x)
+def zip_thresScore_query(z, s):
+    for x in (collection.find({"$and" : [{"address.zipcode" : z}, {"grades.score" : {"$lt" : s}}]})):
+        pprint.pprint(x)
 
-def zip_thresGrade_query(z, g):
+borough_query("Bronx")
+zip_query("10282")
+zip_grade_query("10282", "A")
+zip_thresScore_query("10282", 3)
+# db.restaurants.drop()
