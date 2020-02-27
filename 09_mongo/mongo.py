97ad0417e82ca83deef5e8344052956624587f55
CalfinChoo <cchu00@stuy.edu>
2020-02-26 16:24:30 -0800

    work

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -4,6 +4,21 @@
 #2020-02-26
 
 from pymongo import MongoClient
+import urllib2, json
 
 client = MongoClient()
-db = client.restaurants
+db = client.test
+collection = db.restaurants
+# u = urllib2.urlopen(
+#         "https://raw.githubusercontent.com/WilliamC07/SoftDev/master/09_mongo/primer-dataset.json"
+#     )
+# data = json.loads(u.read())
+f = open("data.json","r")
+response = f.read()
+data = json.loads(response)
+f.close()
+for post in data:
+    collection.insert_one(post)
+
+#
+# print(data)
