2ac678cee0258f2830a8a6583dea5a1fcbe3ff56
Calvin Chu <cchu00@stuy.edu>
2020-02-28 14:54:45 -0500

    cw

---
+++ 10_mongo/app.py
@@ -0,0 +1,20 @@
+#Calvin Chu, Albert Wan - Team Short Circuit
+#SoftDev1 pd9
+#K10: Import/Export Bank
+#2020-02-28
+
+from pymongo import MongoClient
+from bson.json_util import loads
+import pprint
+
+client = MongoClient()
+db = client.db
+collection = db.pokemon
+
+if (db.collection_count() == 0):
+    f = open("data.json","r")
+    response = f.read()
+    data = loads(response)
+    f.close()
+    for post in data:
+        collection.insert_one(post)
